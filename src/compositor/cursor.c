fbb837105f55ec852eed9a106dbcbcad590fe4d6
Keagan Duemmel <kjduemmel@gmail.com>
2026-06-30 12:16:00 -0500

    adjustment on setting xcuror in cursor.c

--- src/compositor/cursor.c
+++ src/compositor/cursor.c
@@ -4,10 +4,10 @@
 
 void server_move_cursor_relative(struct server *server, struct wlr_pointer_motion_event *event) {
     wlr_cursor_move(server->cursor, &event->pointer->base, event->delta_x, event->delta_y);
-    wlr_xcursor_manager_set_cursor_image(server->cursor_mgr, "left_ptr", server->cursor);
+    wlr_cursor_set_xcursor(server->cursor, server->cursor_mgr, "left_ptr");
 }
 
 void server_move_cursor_absolute(struct server *server, struct wlr_pointer_motion_absolute_event *event) {
     wlr_cursor_warp_absolute(server->cursor, &event->pointer->base, event->x, event->y);
-    wlr_xcursor_manager_set_cursor_image(server->cursor_mgr, "left_ptr", server->cursor);
+    wlr_cursor_set_xcursor(server->cursor, server->cursor_mgr, "left_ptr");
 }
